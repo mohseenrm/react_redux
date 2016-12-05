8c1ab5147e0282ddb1f1ee0b5a392dac6e702966
mohseenrm <mohseenmukaddam6@gmail.com>
2016-12-05 01:43:23 -0700

    broadcast

--- src/server.js
+++ src/server.js
@@ -2,4 +2,8 @@ import Server from 'socket.io';
 
 export default function startServer() {
     const io = new Server().attach(8090);
+
+    store.subscribe(
+        () => io.emit('state', store.getState().toJS())
+    );
 }
